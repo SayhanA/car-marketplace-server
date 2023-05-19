9f627628af1b841e94477481c35ec8ce6a8dfabb
SayhanA <sayhanahmed05@gmail.com>
2023-05-19 11:56:36 +0600

    single car by id api url created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -44,6 +44,14 @@ async function run() {
             const car = result.filter(data => data.category === category);
             res.send(car);
             
+        })
+
+        app.get('/car/:id', async(req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = {_id: new ObjectId(id)};
+            const result = await carCollection.find(query).toArray();
+            res.send(result)
         })
         
         // Users
